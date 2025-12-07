82f319ed2cc46c71a2e16e4664877c1d91dcdeb4
Ripcode1 <161825490+Ripcode1@users.noreply.github.com>
2025-12-07 14:40:47 +0200

    Update index.tsx

--- alx-project-0x09/pages/index.tsx
+++ alx-project-0x09/pages/index.tsx
@@ -1,1 +1,18 @@
+import ImageCard from "@/components/common/ImageCard";
+import { ImageProps } from "@/interfaces";
+import { useState } from "react";
 
+const Home: React.FC = () => {
+  const [prompt, setPrompt] = useState<string>("");
+  const [imageUrl, setImageUrl] = useState<string>("");
+  const [generatedImages, setGeneratedImages] = useState<ImageProps[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const handleGenerateImage = async () => {
+    console.log("Generating Image")
+    console.log(process.env.NEXT_PUBLIC_GPT_API_KEY)
+  };
+
+  return (
+    <div className="flex flex-col items-center min-h-screen bg-gray-100 p-4">
+      <div className="flex flex-col items-ce
